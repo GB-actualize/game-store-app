73fe5d026656265db55ab837044eda8fe30a4484
GB-actualize <gbactualize@gmail.com>
2016-09-01 20:15:57 -0500

    got the create function to work.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,8 +2,24 @@ class GamesController < ApplicationController
   def index
     @games = Game.all
   end
+  
+  def new
+  end
 
   def show
     @game = Game.find(params[:id])
   end
+
+  def create
+    @game = Game.create(title: params[:title],
+                        price: params[:price],
+                        genre: params[:genre],
+                 availability: params[:availability])
+  
+   
+
+    render 'show.html.erb'
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get '/' => "games#index"
   get '/games' => 'games#index'
+  
+  get '/games/new' => 'games#new'
+  post '/games' => 'games#create'
+
   get '/games/:id' => 'games#show'
 end
